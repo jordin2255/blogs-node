dcf69336f6ebcc51dfb6d070f7ca4931f956752f
jordin <jordinmathew.mec@gmail.com>
2024-12-06 09:30:22 +1000

    initial commit

---
+++ controllers/feed.js
@@ -0,0 +1,17 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "First Post", content: "This is  first post" }],
+  });
+};
+
+exports.addPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  //create post in DB
+  res
+    .status(201)
+    .json({
+      message: "Post created successfully",
+      post: { id: new Date().toISOString(), title: title, content: content },
+    });
+};
