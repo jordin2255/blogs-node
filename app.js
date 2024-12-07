d09a4c673702f2a8b93d6bd26eff172a600091a2
jordin <jordinmathew.mec@gmail.com>
2024-12-08 09:10:41 +1000

    messages and post model added

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const app=express();
+const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Methods','GET, POST');
-  res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
-const feedRoutes=require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 
-app.use('/feed',feedRoutes);
+app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect('mongodb+srv://jordin:password%40123@cluster0.q8wxp.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0')
+  .then((result) => app.listen(8080))
+  .catch((err) => console.log(err));
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,16 @@
-const express=require('express');
+const express = require("express");
+const { body } = require("express-validator");
 
-const router=express.Router();
+const router = express.Router();
 
-const feedController=require('../controllers/feed');
+const feedController = require("../controllers/feed");
 
-router.get('/posts',feedController.getPosts);
-router.post('/post',feedController.addPost);
+router.get("/posts", feedController.getPosts);
+router.post(
+  "/post",
+  [body("title").trim().isLength({ min: 5 }),
+  body('content').trim().isLength({min:5})],
+  feedController.addPost
+);
 
-module.exports=router;
+module.exports = router;
