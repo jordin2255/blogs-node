1a09f592ba65cfc1da4b6bbfabb0968d7d2f35e5
jordin <jordinmathew.mec@gmail.com>
2024-12-09 19:10:46 +1000

    custom error handling added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path=require('path');
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -5,6 +6,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
+app.use('images',express.static(path.join(__dirname,'images')));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,6 +18,13 @@ app.use((req, res, next) => {
 const feedRoutes = require("./routes/feed");
 
 app.use("/feed", feedRoutes);
+
+app.use((error,req,res,next)=>{
+  console.log(error);
+  const status=error.statusCode || 500;
+  const message=error.message;
+  res.status(status).json({message:message});
+});
 
 mongoose
   .connect('mongodb+srv://jordin:password%40123@cluster0.q8wxp.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0')
--- controllers/feed.js
+++ controllers/feed.js
@@ -21,15 +21,16 @@ exports.getPosts = (req, res, next) => {
 exports.addPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({ message: "Validation failed", errors: errors.array() });
+    const error=new Error('Validation failed');
+    error.statusCode=422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
+    imageUrl:'images/book.jpg',
     creator: { name: "jordin" },
   });
   post
@@ -42,7 +43,10 @@ exports.addPost = (req, res, next) => {
       });
     })
     .catch((err) => {
-      console.log(err);
+      if(!err.statusCode){
+        err.statusCode=500;
+      }
+      next(err);
     });
   
 };
